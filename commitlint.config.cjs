0252b509169afe86a16e2a9368b6b6d46676e66f
OleksandrHridzhak <sgridzak@gmail.com>
2026-07-15 15:58:52 +0300

    chore(global): set new commit rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "test", "refactor", "chore", "docs"],
+    ],
+    "scope-empty": [2, "never"],
+    "scope-enum": [2, "always", ["frontend", "backend", "global"]],
+  },
 };
---
+++ docs/commits.md
@@ -0,0 +1,17 @@
+## Commit messages
+
+We use Conventional Commits:
+https://www.conventionalcommits.org/
+
+Project rules:
+
+- Language: English
+- Format: type(scope): description
+- Scope is required (frontend, backend or global)
+- Allowed types:
+  - feat
+  - fix
+  - test
+  - chore
+  - refactor
+  - docs
